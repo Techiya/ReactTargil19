dd410dd867f647e9a210d2a7ea5f9d51a2ccb3c4
tbrukner <tbrukner@ebay.com>
2020-10-28 15:48:38 +0200

    fix state

--- src/main.js
+++ src/main.js
@@ -3,30 +3,34 @@ import ReactDOM from 'react-dom';
 import _ from 'lodash';
 
 
-function SortableTable ({ data }) {
-    const [ sortedData, setSortedData ] = useState(data);
-    const [ clickedColumn, setClickedColumn ] = useState(0);
-    const [ isDesc, setIsDesc ] = useState(false);
+function SortableTable({ data }) {
+    const [sortedData, setSortedData] = useState(data);
+    const [clickedColumn, setClickedColumn] = useState(0);
+    const [sortDown, setSortrDown] = useState(true);
 
-    
+
     const getIsFirstRow = (row, index) => index === 0;
     const getIsData = (row, index) => index != 0;
 
     function sortByColumn(columnIndex) {
-        const sortDesc = (clickedColumn === columnIndex) ? !isDesc : false;
-        const compareFunction = sortDesc ? (a,b) => (('' + a[columnIndex]).localeCompare(b[columnIndex])) : (a,b) => (('' + b[columnIndex]).localeCompare(a[columnIndex])) ;
+        const currentSortDown = (clickedColumn === columnIndex) ? !sortDown : true;
+        const compareFunction = currentSortDown ? (a, b) => (('' + a[columnIndex]).localeCompare(b[columnIndex])) : (a, b) => (('' + b[columnIndex]).localeCompare(a[columnIndex]));
         const newData = [...data].filter(getIsData).sort(compareFunction);
         const finalData = [...data].filter(getIsFirstRow).concat(newData);
-       
+
         setClickedColumn(columnIndex);
-        setIsDesc(isDesc);
+        setSortrDown(currentSortDown);
         setSortedData(finalData);
+    }
+
+    const createCell = (cell, columnIndex, rowIndex) => {
+        return getIsFirstRow(cell, rowIndex) ? <th key={cell} onClick={e => sortByColumn(columnIndex)}>{cell}</th> : <td key={cell}>{cell}</td>;
     }
 
     return (
         <div>
             <table>
-                {sortedData.map((row, index) => <tr key={index}>{row.map((cell,columnIndex) => <th onClick={columnIndex===0 ? e => sortByColumn(columnIndex) : e => {return;}}>{cell}</th>)}</tr>)}
+                {sortedData.map((row, rowIndex) => <tr key={rowIndex}>{row.map((cell, columnIndex) => createCell(cell, columnIndex, rowIndex))}</tr>)}
             </table>
         </div>
     )
@@ -41,7 +45,7 @@ const App = () => {
         [2, 'anna', 'Israel', 'anna@gmail.com'],
         [3, 'zina', 'UK', 'zina@gmail.com'],
     ];
-    
+
     return <div>
         <SortableTable data={data} />
     </div>
